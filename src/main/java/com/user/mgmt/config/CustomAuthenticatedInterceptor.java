fe00b208aad019ccf216fa96dcd183a92e0e92d5
joseph24x7 <joseph2471994@gmail.com>
2023-07-26 19:50:57 +0530

    Fixing cors issue

--- src/main/java/com/user/mgmt/config/CustomAuthenticatedInterceptor.java
+++ src/main/java/com/user/mgmt/config/CustomAuthenticatedInterceptor.java
@@ -22,6 +22,10 @@ public class CustomAuthenticatedInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
 
+        if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            return true; // Allow preflight OPTIONS request to proceed without authentication
+        }
+
         if (request.getRequestURI().contains("update-user-info")) {
             return true;
         }
