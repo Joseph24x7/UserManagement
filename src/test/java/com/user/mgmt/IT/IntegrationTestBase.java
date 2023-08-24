b917bf15c493cf45222675c82337a056f543540b
joseph24x7 <joseph2471994@gmail.com>
2023-08-24 18:19:36 +0530

    Included the postgres test containers

--- src/test/java/com/user/mgmt/IT/IntegrationTestBase.java
+++ src/test/java/com/user/mgmt/IT/IntegrationTestBase.java
@@ -3,6 +3,7 @@ package com.user.mgmt.IT;
 import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.HashMap;
@@ -14,14 +15,20 @@ import java.util.stream.Stream;
 public class IntegrationTestBase {
 
     public static RedisContainer redisContainer = new RedisContainer(DockerImageName.parse("redis:7.0.11")).withExposedPorts(6379);
-
+    public static PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer("postgres:latest")
+            .withDatabaseName("myuser")
+            .withUsername("myuser")
+            .withPassword("password");
     public static class Initializer {
 
         public static Map<String, Supplier<Object>> initialize() {
-            Stream.of(redisContainer).forEach(GenericContainer::start);
+            Stream.of(redisContainer, postgresqlContainer).forEach(GenericContainer::start);
             Map<String, Supplier<Object>> appProperties = new HashMap<>();
             appProperties.put("spring.data.redis.port", () -> redisContainer.getFirstMappedPort());
             appProperties.put("spring.data.redis.host", () -> redisContainer.getHost());
+            appProperties.put("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+            appProperties.put("spring.datasource.username", postgresqlContainer::getUsername);
+            appProperties.put("spring.datasource.password", postgresqlContainer::getPassword);
             return appProperties;
         }
 
